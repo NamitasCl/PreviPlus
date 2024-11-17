41b438d5ca9e78ee9a77cb810230cdd9ee352abb
Enzo Ramirez <enzo.ramirezs@icloud.com>
2024-11-17 02:56:09 -0300

    Se ajusto que App habia sido deshabilitado temporalmente, se vuelve a su estado original

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,18 +1,16 @@
 import { ChakraProvider } from '@chakra-ui/react'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-//import App from './App.jsx'
+import App from './App.jsx'
 import { UserContextProvider } from './contexto/UserContext.jsx'
-import FormularioAddTrabajador from './PrivateZone/FormularioAddTrabajador'
-import AddNegocio from './PrivateZone/AddNegocio.jsx'
+
 
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <UserContextProvider>
       <ChakraProvider>
-        {/* <App /> */}
-        <AddNegocio />
+        <App />
       </ChakraProvider>
     </UserContextProvider>
   </StrictMode>,
