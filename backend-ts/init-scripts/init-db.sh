478adfa12000ed84329ff0b44ec3c1f96657690d
Enzo Ramirez <enzo.ramirezs@icloud.com>
2024-12-15 23:00:04 -0300

    Se ha borrado carpeta backend-ts por encontrarse con desperfectos

--- backend-ts/init-scripts/init-db.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -e
-
-# Crear base de datos para producción y pruebas
-psql -v ON_ERROR_STOP=1 --username "$POSTGRES_USER" <<-EOSQL
-    CREATE DATABASE previplus;
-    CREATE DATABASE prev-test;
-EOSQL
